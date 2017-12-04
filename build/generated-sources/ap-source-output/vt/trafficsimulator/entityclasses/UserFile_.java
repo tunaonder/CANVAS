8ea919ec36ef1a9bd687349ec1bb3c5e64d2f409
tunaonder <saito@vt.edu>
2017-12-04 16:43:39 -0500

    Mysql Connection Settings updated

--- build/generated-sources/ap-source-output/vt/trafficsimulator/entityclasses/UserFile_.java
+++ build/generated-sources/ap-source-output/vt/trafficsimulator/entityclasses/UserFile_.java
@@ -5,7 +5,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import vt.trafficsimulator.entityclasses.User;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-11-16T18:29:31")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-12-04T16:42:05")
 @StaticMetamodel(UserFile.class)
 public class UserFile_ { 
 
--- build/generated-sources/ap-source-output/vt/trafficsimulator/entityclasses/User_.java
+++ build/generated-sources/ap-source-output/vt/trafficsimulator/entityclasses/User_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import vt.trafficsimulator.entityclasses.UserFile;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-11-16T18:29:31")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-12-04T16:42:05")
 @StaticMetamodel(User.class)
 public class User_ { 
 
