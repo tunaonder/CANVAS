a8e6dad3772549723508e943dd07111d3bbeb7e3
tunaonder <saito@vt.edu>
2017-12-05 23:57:42 -0500

    Server computer websocket connection is set

--- build/generated-sources/ap-source-output/vt/trafficsimulator/entityclasses/UserFile_.java
+++ build/generated-sources/ap-source-output/vt/trafficsimulator/entityclasses/UserFile_.java
@@ -5,7 +5,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import vt.trafficsimulator.entityclasses.User;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-12-05T14:53:12")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-12-05T23:56:51")
 @StaticMetamodel(UserFile.class)
 public class UserFile_ { 
 
--- build/generated-sources/ap-source-output/vt/trafficsimulator/entityclasses/User_.java
+++ build/generated-sources/ap-source-output/vt/trafficsimulator/entityclasses/User_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import vt.trafficsimulator.entityclasses.UserFile;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-12-05T14:53:12")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-12-05T23:56:51")
 @StaticMetamodel(User.class)
 public class User_ { 
 
