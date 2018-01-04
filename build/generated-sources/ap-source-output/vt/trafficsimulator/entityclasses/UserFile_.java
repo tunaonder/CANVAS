cc63bafa35bc12bd4fc2c32b662d5498cd4efabe
tunaonder <saito@vt.edu>
2018-01-04 14:00:34 +0300

    Bootstrap js and css are added

--- build/generated-sources/ap-source-output/vt/trafficsimulator/entityclasses/UserFile_.java
+++ build/generated-sources/ap-source-output/vt/trafficsimulator/entityclasses/UserFile_.java
@@ -5,7 +5,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import vt.trafficsimulator.entityclasses.User;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-01-03T14:46:12")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-01-04T12:38:49")
 @StaticMetamodel(UserFile.class)
 public class UserFile_ { 
 
--- build/generated-sources/ap-source-output/vt/trafficsimulator/entityclasses/User_.java
+++ build/generated-sources/ap-source-output/vt/trafficsimulator/entityclasses/User_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import vt.trafficsimulator.entityclasses.UserFile;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-01-03T14:46:12")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-01-04T12:38:49")
 @StaticMetamodel(User.class)
 public class User_ { 
 
