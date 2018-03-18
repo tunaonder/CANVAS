dc2c4ea243cef83d11f171e1d52105a0bf1653cd
tunaonder <saito@vt.edu>
2018-03-18 11:49:17 -0400

    SetBackgroundButton ajax update is added

--- build/generated-sources/ap-source-output/vt/trafficsimulator/entityclasses/User_.java
+++ build/generated-sources/ap-source-output/vt/trafficsimulator/entityclasses/User_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-18T01:26:10")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-18T11:46:33")
 @StaticMetamodel(User.class)
 public class User_ { 
 
