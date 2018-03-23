5d5321cc445f0ab1266d47dd718ce3dbc977e189
tunaonder <saito@vt.edu>
2018-03-23 00:51:17 -0400

    Project selection is added

--- build/generated-sources/ap-source-output/vt/trafficsimulator/entityclasses/Project_.java
+++ build/generated-sources/ap-source-output/vt/trafficsimulator/entityclasses/Project_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.StaticMetamodel;
 import vt.trafficsimulator.entityclasses.SimulationModel;
 import vt.trafficsimulator.entityclasses.User;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-22T23:41:25")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-23T00:49:56")
 @StaticMetamodel(Project.class)
 public class Project_ { 
 
--- build/generated-sources/ap-source-output/vt/trafficsimulator/entityclasses/User_.java
+++ build/generated-sources/ap-source-output/vt/trafficsimulator/entityclasses/User_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import vt.trafficsimulator.entityclasses.Project;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-22T23:41:25")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-23T00:49:56")
 @StaticMetamodel(User.class)
 public class User_ { 
 
