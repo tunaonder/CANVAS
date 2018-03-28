eeb9d8298312d2279a5ec453af1609df143d0dce
tunaonder <saito@vt.edu>
2018-03-28 00:00:14 -0400

    control panel height is fixed

--- build/generated-sources/ap-source-output/vt/trafficsimulator/entityclasses/SimulationModel_.java
+++ build/generated-sources/ap-source-output/vt/trafficsimulator/entityclasses/SimulationModel_.java
@@ -5,7 +5,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import vt.trafficsimulator.entityclasses.Project;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-27T23:34:13")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-27T23:56:50")
 @StaticMetamodel(SimulationModel.class)
 public class SimulationModel_ { 
 
