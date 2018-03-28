e74f27deb09a1181544d69439d322d0864e3ef8a
tunaonder <saito@vt.edu>
2018-03-27 23:31:41 -0400

    Help and about pages are added

--- build/generated-sources/ap-source-output/vt/trafficsimulator/entityclasses/SimulationModel_.java
+++ build/generated-sources/ap-source-output/vt/trafficsimulator/entityclasses/SimulationModel_.java
@@ -5,7 +5,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import vt.trafficsimulator.entityclasses.Project;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-25T23:01:11")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-27T23:30:52")
 @StaticMetamodel(SimulationModel.class)
 public class SimulationModel_ { 
 
