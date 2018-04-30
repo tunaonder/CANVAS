3ec77f4430fdb052da49099c380a8022425a2ef4
tunaonder <saito@vt.edu>
2018-04-30 11:53:16 -0400

    Favicon is added

--- build/generated-sources/ap-source-output/vt/trafficsimulator/entityclasses/User_.java
+++ build/generated-sources/ap-source-output/vt/trafficsimulator/entityclasses/User_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import vt.trafficsimulator.entityclasses.Project;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-30T11:16:36")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-30T11:51:29")
 @StaticMetamodel(User.class)
 public class User_ { 
 
