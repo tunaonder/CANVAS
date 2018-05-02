fc80ab1793ec0b01eab988f5626265902629a86a
tunaonder <saito@vt.edu>
2018-05-02 17:24:11 -0400

    Message list comparision is updated

--- src/java/vt/trafficnetwork/messaging/helpers/MessageTimeComparator.java
+++ src/java/vt/trafficnetwork/messaging/helpers/MessageTimeComparator.java
@@ -16,16 +16,19 @@ public class MessageTimeComparator implements Comparator<Message> {
     @Override
     public int compare(Message m1, Message m2) {
 
-        JsonObject j1 = m1.getJSONObject();
-        JsonObject j2 = m2.getJSONObject();
-        String t1 = j1.getString("action");
-        String t2 = j2.getString("action");
-
         if (m1.getTime() < m2.getTime()) {
             return -1;
         } else if (m1.getTime() > m2.getTime()) {
             return 1;
-        } else if (t1.equals("changeSpeed") && t2.equals("createVehicle")) {
+        }
+        
+        // Event Time is Equal. Make sure that change speed is placed after vehicle creation
+        JsonObject j1 = m1.getJSONObject();
+        JsonObject j2 = m2.getJSONObject();
+        String t1 = j1.getString("action");
+        String t2 = j2.getString("action");
+        
+        if (t1.equals("changeSpeed") && t2.equals("createVehicle")) {
             return 1;
         }
         else if (t1.equals("createVehicle") && t2.equals("changeSpeed")){
