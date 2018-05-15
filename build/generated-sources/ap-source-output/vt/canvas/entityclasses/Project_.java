2c3fd43d0001fc8d0f1811e38a4126887104b903
tunaonder <saito@vt.edu>
2018-05-15 18:27:26 -0400

    CaseStudy4 sql data is added

--- build/generated-sources/ap-source-output/vt/canvas/entityclasses/Project_.java
+++ build/generated-sources/ap-source-output/vt/canvas/entityclasses/Project_.java
@@ -5,7 +5,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import vt.canvas.entityclasses.User;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-15T16:45:27")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-15T18:22:12")
 @StaticMetamodel(Project.class)
 public class Project_ { 
 
